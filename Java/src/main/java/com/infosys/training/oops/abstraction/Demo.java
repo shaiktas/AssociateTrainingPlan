2af636e7055937d9cab43788853733b0aa0eeeeb
shaiktas <52338042+shaiktas@users.noreply.github.com>
2020-10-27 13:55:17 -0700

    abtraction

---
+++ Java/src/main/java/com/infosys/training/oops/abstraction/Demo.java
@@ -0,0 +1,10 @@
+package com.infosys.training.oops.abstraction;
+
+public class Demo {
+  public static void main(String [] args) {
+    /* Following is not allowed and would raise error */
+    //Employee e = new Employee("George W.", "Houston, TX", 43); //this will error out
+    System.out.println("\n Call mailCheck using Employee reference--");
+    //e.mailCheck();
+  }
+}
