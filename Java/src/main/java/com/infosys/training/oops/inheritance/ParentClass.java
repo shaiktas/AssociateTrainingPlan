58d08d212564af8bd7bd1ac3a561b6cca1b57a60
shaiktas <52338042+shaiktas@users.noreply.github.com>
2020-10-27 13:58:58 -0700

    Add files via upload

---
+++ Java/src/main/java/com/infosys/training/oops/inheritance/ParentClass.java
@@ -0,0 +1,14 @@
+package com.infosys.training.oops.inheritance;
+
+public class ParentClass {
+
+  public int a = 10;
+  public int b = 20;
+
+  public void common_1(){
+    System.out.println("Hey i am from Parent and common_1");
+  }
+  public void common_2(){
+    System.out.println("Hey i am from Parent and common_2");
+  }
+}
