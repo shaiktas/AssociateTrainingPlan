40f0165dc84bc4cf260b048e7bc94780c1a9fa20
Tirthankar Parija <54262291+TirthankarParija@users.noreply.github.com>
2020-10-28 20:55:07 +0530

    Create ConcurrencyProblem.java

---
+++ Java/src/main/java/com/infosys/training/multi-threading/ConcurrencyProblem.java
@@ -0,0 +1,38 @@
+package multi.threading.example;
+
+public class ConcurrencyProblem {
+
+	static int[] array = { 0 };
+	public static void main(String[] args) throws InterruptedException {
+
+		final ConcurrencyProblem concurrencyProblem = new ConcurrencyProblem();
+
+		Thread a = new Thread("A") {
+			public void run() {
+				for (int i = 1; i <= 1000; i++) {
+					concurrencyProblem.increase();
+				}
+			}
+		};
+		
+		Thread b = new Thread("B") {
+			public void run() {
+				for (int i = 1; i <= 1000; i++) {
+					concurrencyProblem.decrease();
+				}
+			}
+		};
+		a.start();
+		b.start();
+		a.join();
+		b.join();
+		System.out.println(array[0]);
+	}
+	public synchronized void increase() { //synchronized
+		array[0]++;
+	}
+	public synchronized void decrease() {
+		array[0]--;
+	}
+
+}
